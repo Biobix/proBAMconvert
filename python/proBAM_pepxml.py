35e803861e9fd77a83081b7cc71c9e8eb7a19a2a
Geo <gbadran_90@live.com>
2019-02-03 21:18:22 +0100

    Updating proBAM_pepxml.py: fixing the function get_enzyme_pepxml() infinit loop, enhancing the parsing with the lxml library. Updating the function get_enzyme_specificity_pepxml() to use lxml for the parsing.

--- python/proBAM_pepxml.py
+++ python/proBAM_pepxml.py
@@ -16,6 +16,9 @@
 ##############################################################################
 from pyteomics import pepxml
 from fnmatch import fnmatch
+from lxml import etree
+
+
 #
 # Reads pepXML and stores variables in an hash
 #
@@ -32,10 +35,14 @@ def get_PSM_pepxml(psm_file):
         # adjust search scores
         if 'search_hit' in row.keys():
             for search_hit in row['search_hit']:
-                search_hit['massdiff']=str(search_hit['massdiff'])+';'+str(search_hit['calc_neutral_pep_mass'])+';'+str(row['precursor_neutral_mass'])
-                search_hit['search_score']={'score':_get_score_(search_hit['search_score']),
-                                                        'evalue':_get_evalue_(search_hit['search_score'])}
-
+                search_hit['massdiff'] = str(
+                    search_hit['massdiff']) + ';' + str(
+                        search_hit['calc_neutral_pep_mass']) + ';' + str(
+                            row['precursor_neutral_mass'])
+                search_hit['search_score'] = {
+                    'score': _get_score_(search_hit['search_score']),
+                    'evalue': _get_evalue_(search_hit['search_score'])
+                }
 
         # if count==5500:
         #    break
@@ -43,6 +50,8 @@ def get_PSM_pepxml(psm_file):
         # count+=1
     del PEP
     return PSM
+
+
 #
 # Attempts to extract enzyme information from the pepXML file
 #
@@ -51,51 +60,40 @@ def get_enzyme_pepxml(psm_file):
     :param psm_file: psm_file
     :return: enzyme
     '''
-    f=open(psm_file,'r')
-    count=0
-    while count <100:
-        line=f.readline()
-        if "enzyme" not in line:
-            continue
-        else:
-            line=line.replace('-','_')
-            line=line.lower()
-            if fnmatch(line,'*trypsin?p*'):
-                return 2
-                break
-            elif fnmatch(line,'*trypsin*'):
-                return 1
-                break
-            elif fnmatch(line, '*lys?c*'):
-                return 3
-                break
-            elif fnmatch(line, '*lys?n*'):
-                return 4
-                break
-            elif fnmatch(line, '*arg?c*'):
-                return 5
-                break
-            elif fnmatch(line, '*asp?n*'):
-                return 6
-                break
-            elif fnmatch(line, '*cnbr*'):
-                return 7
-                break
-            elif fnmatch(line, '*glu?c*'):
-                return 8
-                break
-            elif fnmatch(line, '*pepsina*'):
-                return 9
-                break
-            elif fnmatch(line, '*chymotrypsin*'):
-                return 10
-                break
-            elif fnmatch(line, '*no?enzyme*'):
-                return 0
-                break
-    return "-1"
+    try:
+        root = etree.parse(psm_file).getroot()
+        namespace = root.nsmap.items()[0][1]
+        line = root.xpath(
+            "//ns:sample_enzyme", namespaces={'ns': namespace})[0].get("name")
+        line = line.replace('-', '_')
+        line = line.lower()
+        if fnmatch(line, '*trypsin?p*'):
+            return 2
+        elif fnmatch(line, '*trypsin*'):
+            return 1
+        elif fnmatch(line, '*lys?c*'):
+            return 3
+        elif fnmatch(line, '*lys?n*'):
+            return 4
+        elif fnmatch(line, '*arg?c*'):
+            return 5
+        elif fnmatch(line, '*asp?n*'):
+            return 6
+        elif fnmatch(line, '*cnbr*'):
+            return 7
+        elif fnmatch(line, '*glu?c*'):
+            return 8
+        elif fnmatch(line, '*pepsina*'):
+            return 9
+        elif fnmatch(line, '*chymotrypsin*'):
+            return 10
+        elif fnmatch(line, '*no?enzyme*'):
+            return 0
+        return "-1"
+    except:
+        return "-1"
 
-#
+
 # Attempts to extract enzyme specificity information from the pepXML file
 #
 def get_enzyme_specificity_pepxml(psm_file):
@@ -103,26 +101,23 @@ def get_enzyme_specificity_pepxml(psm_file):
     :param psm_file: psm file
     :return: enzyme specificity
     '''
-    f=open(psm_file,'r')
-    count=0
-    while count <100:
-        count+=1
-        line=f.readline()
-        if "enzyme" not in line:
-            continue
-        else:
-            line=line.replace('-','_')
-            line=line.lower()
-            if fnmatch(line,'*non*enzy*'):
-                return 0
-                break
-            elif fnmatch(line,'*semi*enzy*'):
-                return 1
-                break
-            elif fnmatch(line, '*fully*enzy*'):
-                return 2
-                break
-    return 3
+    try:
+        root = etree.parse(psm_file).getroot()
+        namespace = root.nsmap.items()[0][1]
+        line = root.xpath(
+            "//ns:specificity", namespaces={'ns': namespace})[0].get("cut")
+        line = line.replace('-', '_')
+        line = line.lower()
+        if fnmatch(line, '*non*enzy*'):
+            return 0
+        elif fnmatch(line, '*semi*enzy*'):
+            return 1
+        elif fnmatch(line, '*fully*enzy*'):
+            return 2
+        return 3
+    except:
+        return 3
+
 
 #
 # extract search summary information from pepXML
@@ -132,18 +127,20 @@ def extract_comments_from_pepxml(psm_file):
     :param psm_file: psm_file
     :return: comments array
     '''
-    f=open(psm_file,'r')
-    count=0
-    comments=[]
-    while count <200:
-        count+=1
-        line=f.readline()
+    f = open(psm_file, 'r')
+    count = 0
+    comments = []
+    while count < 200:
+        count += 1
+        line = f.readline()
         if "search_summary" in line:
             while "/search_summary" not in line:
                 comments.append(line)
-                line=f.readline()
+                line = f.readline()
             return comments
     return comments
+
+
 #
 # retrieve score for a psm
 #
@@ -161,9 +158,11 @@ def _get_score_(search_score):
             hit = 1
             hit_key = key
     if hit == 1:
-        return  search_score[hit_key]
+        return search_score[hit_key]
     else:
         return "-1"
+
+
 #
 # retrieve the evalue for a psm
 #
